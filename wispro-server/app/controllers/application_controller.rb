792193b57ed01536e4c123393c474ba8a5f41fca
MartinCarrion01 <martincarrion2864212@gmail.com>
2023-01-10 00:05:35 -0300

    cambios...

--- wispro-server/app/controllers/application_controller.rb
+++ wispro-server/app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::API
             decoded = jwt_decode(token)
             @current_client = Client.find(decoded[:client_id])
         rescue ActiveRecord::RecordNotFound => exception
-            render json: { errors: exception.message }, status: :unauthorized
+            render json: { message: "No se pudo encontrar un cliente usando el token de autorizacion" }, status: :unauthorized
         rescue JWT::DecodeError => exception
-            render json: { errors: exception.message }, status: :unauthorized
+            render json: { message: exception.message }, status: :unauthorized
         end
     end
 
@@ -19,9 +19,9 @@ class ApplicationController < ActionController::API
             decoded = jwt_decode(token)
             @current_provider = Provider.find(decoded[:provider_id])
         rescue ActiveRecord::RecordNotFound => exception
-            render json: { errors: exception.message }, status: :unauthorized
+            render json: { message: "No se pudo encontrar un proveedor usando el token de autorizacion" }, status: :unauthorized
         rescue JWT::DecodeError => exception
-            render json: { errors: exception.message }, status: :unauthorized
+            render json: { message: exception.message }, status: :unauthorized
         end
     end
 
